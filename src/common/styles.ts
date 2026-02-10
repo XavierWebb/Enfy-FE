cc8e402c4778b010fc4b570ece198a6929cce71c
kali <xaviernavarro2017@gmail.com>
2026-02-10 14:22:08 -0300

    landing

---
+++ src/common/styles.ts
@@ -0,0 +1,9 @@
+export const darkMode_palette = {
+    green: '#1E6028',
+    light_green: '#339E43',
+};
+
+export const lightMode_palette = {
+    green: '#1E6028',
+    light_green: '#339E43',
+};
---
+++ src/components/texts.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Tittle_One = styled.h1`
+    color: white;
+    font-family: 'Jost', sans-serif;
+    font-size: 2.5rem;
+    font-weight: 800;
+`
+
+export const Text_One = styled.p`
+    color: white;
+    font-family: 'Jost', sans-serif;
+    font-size: 1.5rem;
+`
