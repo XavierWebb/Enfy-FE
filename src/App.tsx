d65076589b483cdcd7faf3678df67a7d8626f9a6
kali <xaviernavarro2017@gmail.com>
2026-04-18 11:02:35 -0300

    Cookie Cleaner

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,41 @@ import { useEffect } from 'react'
 import i18n from './i18n'
 import { SearchPage } from './pages/searchPage'
 import { AdminPage } from './pages/adminPage'
+import { useAppDispatch } from './redux/hooks'
 
 
 function App() {
   const user = useSelector((state: RootState) => state.users.currentUser)
   const mode = useSelector((state: RootState) => state.users.currentUser.mode)
   const language = user.language || 'en'
-
+  const dispatch = useAppDispatch();
   useEffect(() => {
-  if (i18n.language !== language) {
-    i18n.changeLanguage(language)
-  }
+    if (i18n.language !== language) {
+      i18n.changeLanguage(language)
+    }
 
-  document.documentElement.dir = language === 'ar' ? 'rtl' : 'ltr'
-}, [language])
+    document.documentElement.dir = language === 'ar' ? 'rtl' : 'ltr'
+  }, [language])
 
   useEffect(() => {
     document.body.classList.remove('dark', 'light');
     document.body.classList.add(mode);
   }, [mode])
 
+  useEffect(() => {
+    const getCookie = (name: string) => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop()?.split(';').shift();
+      return null;
+    };
 
+    const accessToken = getCookie("access_token");
+
+    if (!accessToken) {
+      localStorage.clear();
+    }
+  }, [dispatch])
   return (
     <>
       <ToastContainer theme='dark' />
